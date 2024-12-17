f945c83de3df6aae02cef61e57473b4114fd5fc8
Syed Abdullah <160462766+syedabdullahuddin@users.noreply.github.com>
2024-12-17 20:17:31 +0500

    Add files via upload

---
+++ style.js
@@ -0,0 +1,84 @@
+// Chapter 1 
+
+// Question 1
+// alert("Js Alert Box") 
+
+// Question 2
+// alert("Error! please enter a valaid password.")
+
+// Question 3
+//alert("Welcome to Js Land...\n Happy Coding!") 
+
+// Question 4
+// alert("Welcome to Js land...")
+// alert("Happy Coding \n Prevent this page to additional dailouge")
+
+// Question 5
+// Console.log("Hello I can run JS through my web browser's console");
+
+// Chapter 2
+
+//Question 1
+// var username;
+
+// Question 2
+// var myName = "Syed Abdullah";
+
+// Question 3
+// var message;
+// message = "Hello World";
+// alert(message);
+
+// Question 4
+// alert("My name is Syed Abudllah")
+// alert("I'm 18 yers old")
+// alert("Certified Web Developer")
+
+// Question 5
+// alert("PIZZA \n PIZZ \n PIZ \n PI \n P")
+
+// Question 6
+// var email;
+// email="abdulsyeddd.11@gmail.com";
+// alert(email)
+
+// Question 7
+// var book;
+// book=" “A smarter way to learn JavaScript”.";
+// alert(book)
+
+// Question 9 
+// alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+
+// Chapter 3
+
+// Question 1
+//  var age;
+//  age=18;
+//  alert("My age is "+age)
+
+// Question 2
+// let visitCount = localStorage.getItem('visitCount');
+
+// // Agar visitCount nahi hai toh initialize karen
+// if (visitCount === null) {
+//     visitCount = 1;
+// } else {
+//     // Agar visitCount hai toh increment karen
+//     visitCount = parseInt(visitCount) + 1;
+// }
+
+// // Update the visit count in localStorage
+// localStorage.setItem('visitCount', visitCount);
+
+// // Show the visit count on the page
+// window.onload = function() {
+//     document.write ("You have visited this site " + visitCount + " times.")
+// };
+
+// Question 3
+// var birthYear
+// birthYear="2006";
+// document.write("My birth year is "+birthYear)
+
+
